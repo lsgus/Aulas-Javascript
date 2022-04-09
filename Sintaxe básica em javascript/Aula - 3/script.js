4398c57c207a379e0c0cf11b7bb7680b52508248
Gustav <yorugus@icloud.com>
2022-04-08 23:33:38 -0300

    Aula - 3

--- Sintaxe básica em javascript/Aula - 3/script.js
+++ Sintaxe básica em javascript/Aula - 3/script.js
@@ -1,5 +1,49 @@
 // o que são vetores ou arrays
 
 // como declarar um array
-let array = ['string', 1, true];
-console.log(array);
+/*let array = ['string', 1, true];
+console.log(array); */
+
+
+// pode guardar vários tipos de dados
+
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+/*console.log(array);*/
+
+// forEach
+/*array.forEach(function(item, index){console.log(item, index)});*/
+
+// push
+
+/*array.push('novo item');
+console.log(array);*/
+
+/*array.pop();
+console.log(array);*/
+
+/*array.shift();
+console.log(array);*/
+
+/*array.unshift('novo item no inicio');
+console.log(array);*/
+
+/*console.log(array.indexOf(true));*/
+
+/*array.splice(0,3);
+console.log(array);*/
+
+/*let novoArray = array.slice(0, 3);
+console.log(novoArray);*/
+
+let object = {string: 'string', number: 1, Boolean: true, array: ['array'], objectInterno:
+ {objectInterno: 'objeto interno' }};
+console.log(object.Boolean);
+
+var string = object.string;
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno);
+
+var {string, Boolean, objectInterno} = object;
+console.log(string, Boolean, objectInterno);
